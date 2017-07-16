597d2a37f979d55f507d267cc7feed944b4aa4e3
Dardan <mail@dardan.im>
2017-07-16 06:16:29 +0200

    Implemented the video div

--- src/components/QuestionItem/QuestionItem.js
+++ src/components/QuestionItem/QuestionItem.js
@@ -1,23 +1,32 @@
+// @flow
+
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import PropTypes from 'prop-types';
 
 import s from './QuestionItem.css';
 
 const FaShareAlt = require('react-icons/lib/fa/share-alt');
 
+type Question = { title: string, id: string };
+
 class QuestionItem extends React.Component {
-  static propTypes = {
-    question: PropTypes.shape({ title: PropTypes.string, id: PropTypes.string })
-      .isRequired,
+  props: {
+    question: Question,
+    openModal: Question => void,
   };
+
   render() {
     return (
       <li className={s.questionItem}>
         <div className={s.iconWrapper}>
           <FaShareAlt />
         </div>
-        <a href={`#${this.props.question.id}`}>
+        <a
+          href={`#${this.props.question.id}`}
+          onClick={() => {
+            this.props.openModal(this.props.question);
+          }}
+        >
           {this.props.question.title}
         </a>
       </li>
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Modal from 'react-modal';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import PropTypes from 'prop-types';
 
 import QuestionItem from '../QuestionItem';
 
 import s from './Questions.css';
+
+const customStyles = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  },
+  content: {
+    position: 'absolute',
+    padding: '0px',
+    width: '800px',
+    height: '500px',
+    margin: '0px auto',
+    background: '#fff',
+    WebkitOverflowScrolling: 'touch',
+    overflow: 'hidden',
+    borderRadius: '0px',
+    border: '0px',
+  },
+};
 
 class Questions extends React.Component {
   static propTypes = {
@@ -17,12 +41,52 @@ class Questions extends React.Component {
     ).isRequired,
   };
 
+  state = {
+    current: null,
+  };
+
+  openModal = question => {
+    this.setState({ current: question });
+  };
+
+  afterOpenModal = () => {
+    // Do something else
+  };
+
+  closeModal = () => {
+    this.setState({ current: null });
+  };
+
+  embedLink = id => `https://www.youtube.com/embed/${id}`;
+
   render() {
     return (
       <div className={s.questions}>
         {this.props.questions.map(question =>
-          <QuestionItem question={question} />,
+          <QuestionItem question={question} openModal={this.openModal} />,
         )}
+
+        <Modal
+          isOpen={this.state.current}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Answer Video"
+        >
+          <iframe
+            title={
+              this.state.current ? this.state.current.title : 'No opened video'
+            }
+            src={
+              this.state.current
+                ? this.embedLink(this.state.current.videoUrl)
+                : 'no-link'
+            }
+            width={customStyles.content.width}
+            height="400px"
+            allowFullScreen
+          />
+        </Modal>
       </div>
     );
   }
