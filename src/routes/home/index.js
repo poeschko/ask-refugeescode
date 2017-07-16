95ea8fba2faf4c7847d0e3d49459bcc2a50acfd8
Jan Poeschko <jan@poeschko.com>
2017-07-16 05:26:00 +0200

    remove obsolete code in home/index.js

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,15 +3,6 @@ import Home from './Home';
 import Layout from '../../components/Layout';
 
 async function action() {
-  // const resp = await fetch('/graphql', {
-  //   body: JSON.stringify({
-  //     query: '{questions{id,title,videoUrl},me{email}}',
-  //   }),
-  // });
-  // const { data } = await resp.json();
-  // if (!data || !data.questions || !data.me) {
-  //   throw new Error('Failed to load data.');
-  // }
   return {
     chunks: ['home'],
     title: 'Ask refugees{code}',
